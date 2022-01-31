07881f59040c953b74f8f4faf54d72d673a2b95e
qwerewqwerew <qwerew0@gmail.com>
2022-01-31 20:15:56 +0900

    슬라이드 구현

--- src/main/index.js
+++ src/main/index.js
@@ -4,12 +4,15 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import {API_URL} from '../config/constants.js';
+import {API_URL} from '../config/constants.js'
+import {Carousel} from "antd"
+
 
 dayjs.extend(relativeTime);
 
 function MainPage() {
 	const [products, setProducts] = React.useState([]);
+	const [banners, setBanners] = React.useState([]);
 	React.useEffect(function () {
 		axios
 			.get(`${API_URL}/products/`)
@@ -20,13 +23,29 @@ function MainPage() {
 			.catch(function (error) {
 				console.log("에러발생:", error);
 			});
-		a
+		axios
+			.get(`${API_URL}/banners`)
+			.then((result) => {
+				const banners = result.data.banners;
+				setBanners(banners);
+			})
+			.catch((error) => {
+				console.error("에러 발생 : ", error);
+			});
 	}, []);
 	return (
 		<div>
-			<div id="banner">
-				<img src="images/banners/banner1.png" alt="" />
-			</div>
+			<Carousel autoplay autoplaySpeed={3000}>
+				{banners.map((banner, index) => {
+					return (
+						<Link to={banner.href}>
+							<div id="banner">
+								<img src={`${API_URL}/${banner.imageUrl}`} alt=""/>
+							</div>
+						</Link>
+					);
+				})}
+			</Carousel>
 			<h1>Products</h1>
 			<div id="product-list">
 				{products.map(function (product, index) {
@@ -34,19 +53,17 @@ function MainPage() {
 						<div className="product-card">
 							<Link className="product-link" to={`/products/${product.id}`}>
 								<div>
-									<img className="product-img" src={`${API_URL}/${product.imageUrl}`} />
+									<img className="product-img" src={`${API_URL}/${product.imageUrl}`} alt="" />
 								</div>
 								<div className="product-contents">
 									<span className="product-name">{product.name}</span>
 									<span className="product-price">{product.price}</span>
 									<div class="product-footer">
 										<div className="product-seller">
-											<img className="product-avatar" src="images/icons/avatar.png" />
+											<img className="product-avatar" src="images/icons/avatar.png" alt=""/>
 											<span>{product.seller}</span>
 										</div>
-										<span className="product-date">
-											{dayjs(product.createdAt).fromNow()}	
-										</span>
+										<span className="product-date">{dayjs(product.createdAt).fromNow()}</span>
 									</div>
 								</div>
 							</Link>
