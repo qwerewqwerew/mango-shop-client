3897ac1461d1029e000f3121a1fe432e2181e153
qwerewqwerew <qwerew0@gmail.com>
2022-01-31 19:29:15 +0900

    서버 파일 처리 (multer) & 망고마켓 구현 완료

--- src/products/index.js
+++ src/products/index.js
@@ -2,13 +2,15 @@ import "./style.css";
 import {useParams} from "react-router-dom";
 import axios from "axios";
 import {useEffect, useState} from "react";
+import {API_URL} from '../config/constants.js'
+import dayjs from 'dayjs';
 
 function ProductPage() {
 	const {id} = useParams();
 	const [product, setProduct] = useState(null);
 	useEffect(function () {
 		axios
-			.get(`http://localhost:8080/products/${id}`)
+			.get(`${API_URL}/products/${id}`)
 			.then(function (result) {
 				setProduct(result.data.product);
 			})
@@ -22,7 +24,7 @@ function ProductPage() {
 	return (
 		<div>
 			<div id="image-box">
-				<img src={"/" + product.imageUrl} alt={product.name} />
+				<img src={`${API_URL}/${product.imageUrl}`} alt={product.name} />
 			</div>
 			<div id="profile-box">
 				<img src="/images/icons/avatar.png" alt={product.seller} />
@@ -31,8 +33,8 @@ function ProductPage() {
 			<div id="contents-box">
 				<div id="name">{product.name}</div>
 				<div id="price">{product.price}원</div>
-				<div id="createAt">2022.01.15</div>
-				<div id="description">{product.description}</div>
+				<div id="createAt">{dayjs(product.createdAt).format('YYYY년 MM월 DD일')}</div>
+				<pre id="description">{product.description}</pre>
 			</div>
 		
 		</div>
